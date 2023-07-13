9e3193c82603e856e133676703bff360f056c6ab
Roobadharani <127882154+Roobadharani@users.noreply.github.com>
2023-07-13 15:54:42 +0530

    Add files via upload

---
+++ app.js
@@ -0,0 +1,6 @@
+//getting response from indexhtml by using id name;
+var startButton = document.getElementById("start");
+//when click it navigates to game.html page
+startButton.onclick = () => {
+    location.href = "./game.html";
+}
